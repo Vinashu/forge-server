07a38ed1ea2177664e93aefe7097b10e07785170
Rogerio de Leon Pereira <vinashu@gmail.com>
2022-08-17 09:19:51 -0500

    Added some error handler

--- backend/server.js
+++ backend/server.js
@@ -33,12 +33,41 @@ if(process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'));
 } else {
     app.get('/', (req, res) => {
-        res.status(200).json({message: "Welcome to the Support Desk API"});
+        res.status(200).json({message: "Welcome to the FORGE framework"});
      });    
 }
 
 // Middleware
 app.use(errorHandler);
+
+app.use(function(req, res, next) {
+    res.status(404);
+  
+    // respond with html page
+    if (req.accepts('html')) {
+      res.render('404', { url: req.url });
+      return;
+    }
+  
+    // respond with json
+    if (req.accepts('json')) {
+      res.json({ error: 'Not found' });
+      return;
+    }
+  
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
+
+process.on('uncaughtException', (error)  => {
+    console.log('Alert! ERROR : ',  error);
+    process.exit(1); // Exit your app 
+});
+
+process.on('unhandledRejection', (error, promise)  => {
+    console.log('Alert! ERROR : ',  error);
+    process.exit(1); // Exit your app 
+});
 
 app.listen(PORT, () => {
     console.log(`Server successfully running on ${PORT}`.yellow);
